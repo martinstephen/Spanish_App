badd9988efa0221717ffe0b8913f0d3c5e14763a
martinstephen <smartinsemail@gmail.com>
2023-04-16 15:36:08 +0200

    random pronoun and root function

--- Spanish_Verbs/Spanish_Verbs.cpp
+++ Spanish_Verbs/Spanish_Verbs.cpp
@@ -4,20 +4,41 @@
 #include <iostream>
 #include <vector>
 #include "Spanish_Verbs.hpp"
+#include <cstdlib>
+#include <ctime>
 //using namespace std;
 
 class Verbs {
+public:
     std::vector <std::string> pronouns = { "Yo", "Tu", "El / Elle / Usted", "Nosostros / as", "Vosostros / as", "Ellos / Ellas / Ustedes" };
     std::vector <std::string> verbs_regular;
     std::vector <std::string> verbs_nonregular;
-    std::vector <std::string> AR_Congrugation( std::string Verb );
+    std::string AR_Congrugation( std::string Verb, std::string Pronoun );
     std::vector <std::string> IR_Congrugation( std::string Verb );
     std::vector <std::string> ER_Congrugation( std::string Verb );
 
+
 };
+std::string get_regular_root(std::string Verb)
+{
+    Verb.pop_back();
+    Verb.pop_back();
+    return Verb;
+
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    srand(time(0));
+    Verbs Test;
+    int index;
+    for (int i = 0; i< 10; i++)
+    {
+         index = rand() % 6;
+         std::cout << Test.pronouns[index] << std::endl;
+    }
+    get_regular_root("cambinar");
+    get_regular_root("hablar");
 }
 
