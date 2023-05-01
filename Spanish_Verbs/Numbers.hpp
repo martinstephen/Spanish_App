1bd838a2b50527abe16a33b83e8e04d0c94c67d6
martinstephen <smartinsemail@gmail.com>
2023-05-01 11:49:21 +0200

    added numbers

---
+++ Spanish_Verbs/Numbers.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class numbers {
+  vector<pair<int, string>> vec;
+  map<int, string> translations1 = {{1, "uno"},
+                                    {2, "dos"},
+                                    {3, "tres"},
+                                    {4, "cuatro"},
+                                    {5, "cinco"},
+                                    {6, "seis"},
+                                    {7, "siete"},
+                                    {8, "ocho"},
+                                    {9, "nueve"},
+                                    {10, "diez"},
+                                    {11, "once"},
+                                    {12, "doce"},
+                                    {13, "trece"},
+                                    {14, "catorce"},
+                                    {15, "quince"},
+                                    {16, "dieciséis"},
+                                    {17, "diecisiete"},
+                                    {18, "dieciocho"},
+                                    {19, "diecinueve"},
+                                    {20, "veinte"},
+                                    {30, "treinta"},
+                                    {40, "cuarenta"},
+                                    {50, "cincuenta"},
+                                    {60, "sesenta"},
+                                    {70, "setenta"},
+                                    {80, "ochenta"},
+                                    {90, "noventa"},
+                                    {100, "cien"},
+                                    {200, "doscientos"},
+                                    {300, "trescientos"},
+                                    {400, "cuatrocientos"},
+                                    {500, "quinientos"}}; 
+};
