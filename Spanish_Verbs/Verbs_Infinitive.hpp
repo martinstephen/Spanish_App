e45238ca8bb896c46de6c08de13798294764a843
martinstephen <smartinsemail@gmail.com>
2023-05-05 16:24:01 +0200

    added more verbs

--- Spanish_Verbs/Verbs_Infinitive.hpp
+++ Spanish_Verbs/Verbs_Infinitive.hpp
@@ -10,9 +10,20 @@ public:
   std::string ER_ending = "er";
   std::string IR_ending = "ir";
   int Get_last_2_Letters = 2;
-  std::map<std::string, std::string>verbs_regular = {
-      {"To Speak", "Hablar"},
-      {"To Eat", "Comer"}, 
-      {"To Live", "Vivir"}};
+  std::map<std::string, std::string> verbs_regular = {
+          
+                  {"to say", "decir"},
+                  {"to come", "venir"},
+          {"to see", "ver"},         {"to know", "saber"},
+          {"to want", "querer"},     {"to like", "gustar"},
+          {"to love", "amar"},       {"to speak", "hablar"},
+          {"to work", "trabajar"},   {"to study", "estudiar"},
+          {"to play", "jugar"},      {"to live", "vivir"},
+          {"to eat", "comer"},       {"to drink", "beber"},
+          {"to sleep", "dormir"},    {"to run", "correr"},
+          {"to walk", "caminar"},    {"to swim", "nadar"},
+          {"to read", "leer"},       {"to write", "escribir"},
+          {"to listen", "escuchar"}, {"to sing", "cantar"},
+          {"to dance", "bailar"}};
   std::vector<std::string> verbs_nonregular = {"Estar", "Ir"};
 };
