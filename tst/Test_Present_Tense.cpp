46b237cd4fcbfb46dd47932eed0b058b603ecc49
martinstephenson <smartinsemail@gmail.com>
2025-10-06 20:08:10 +0200

    fixed UT

--- tst/Test_Present_Tense.cpp
+++ tst/Test_Present_Tense.cpp
@@ -6,6 +6,19 @@ TEST_CASE("AR_Congrugation_Present_Tense returns correct conjugation", "[present
     REQUIRE(pt.AR_Congrugation_Present_Tense("hablar", 0) == "hablo");
     REQUIRE(pt.AR_Congrugation_Present_Tense("hablar", 1) == "hablas");
     REQUIRE(pt.AR_Congrugation_Present_Tense("hablar", 2) == "habla");
+    REQUIRE(pt.AR_Congrugation_Present_Tense("trabajar", 3) == "trabajamos");
+    REQUIRE(pt.AR_Congrugation_Present_Tense("cantar", 4) == "cantáis");
+    REQUIRE(pt.AR_Congrugation_Present_Tense("bailar", 5) == "bailan");
+}
+
+TEST_CASE("ER_Congrugation_Present_Tense returns correct conjugation", "[present_tense]") {
+    Verbs_Present_Tense pt;
+    REQUIRE(pt.ER_Congrugation_Present_Tense("comer", 0) == "como");
+    REQUIRE(pt.ER_Congrugation_Present_Tense("comer", 1) == "comes");
+    REQUIRE(pt.ER_Congrugation_Present_Tense("comer", 2) == "come");
+    REQUIRE(pt.ER_Congrugation_Present_Tense("beber", 3) == "bebemos");
+    REQUIRE(pt.ER_Congrugation_Present_Tense("leer", 4) == "leéis");
+    REQUIRE(pt.ER_Congrugation_Present_Tense("correr", 5) == "corren");
 }
 
 TEST_CASE("IR_Congrugation_Present_Tense returns correct conjugation", "[present_tense]") {
@@ -13,4 +26,16 @@ TEST_CASE("IR_Congrugation_Present_Tense returns correct conjugation", "[present
     REQUIRE(pt.IR_Congrugation_Present_Tense("vivir", 0) == "vivo");
     REQUIRE(pt.IR_Congrugation_Present_Tense("vivir", 1) == "vives");
     REQUIRE(pt.IR_Congrugation_Present_Tense("vivir", 2) == "vive");
+    REQUIRE(pt.IR_Congrugation_Present_Tense("escribir", 3) == "escribimos");
+    REQUIRE(pt.IR_Congrugation_Present_Tense("abrir", 4) == "abrís");
+    REQUIRE(pt.IR_Congrugation_Present_Tense("recibir", 5) == "reciben");
+}
+
+TEST_CASE("Irregular verbs in present tense", "[present_tense]") {
+    Verbs_Present_Tense pt;
+    REQUIRE(pt.AR_Congrugation_Present_Tense("Estar", 0) == "Estoy");
+    REQUIRE(pt.AR_Congrugation_Present_Tense("Dar", 1) == "Das");
+    REQUIRE(pt.AR_Congrugation_Present_Tense("Pensar", 2) == "Piensa");
+    REQUIRE(pt.IR_Congrugation_Present_Tense("ir", 0) == "Voy");
+    REQUIRE(pt.IR_Congrugation_Present_Tense("Decir", 1) == "Dices");
 }
